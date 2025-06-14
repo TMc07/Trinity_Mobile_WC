64d8d3f2dd3cdc5679d0f4b20c8a42c553fd058f
Ty M <tymcclure@gmail.com>
2025-06-13 23:23:46 -0500

    Pushing updates 06-13-2025

--- Patient_itemized.py
+++ Patient_itemized.py
@@ -1,8 +1,43 @@
 import pandas as pd
 import datetime
 import re
+from request_Script import main
+import os
 
-patient_data = pd.read_csv('monday_export_20250612_235215.csv')
+# Creates the timestamp for current days information
+timestamp = datetime.datetime.now().strftime("%Y%m%d")
+
+current_TimeStamp = main()
+
+combined_boards = {}
+
+# Boards of interest are 8585885825 which is Billing, 8586310441 which is the Census Board, 9023723118 which is Scheduling - Texas, 9023703555 is Incoming Referrals - Texas
+board_name = {
+    '8585885825': 'Billing',
+    '8586310441': 'Census',
+    '9023723118': 'Scheduling - Texas',
+    '9023703555': 'Incoming Referrals - Texas'
+}
+
+for board_id, patient_data in current_TimeStamp:
+    board_id_str = str(board_id)
+    
+    if board_id_str not in board_name:
+        print(f"Unknown board ID {board_id}, skipping...")
+        continue
+    board_group = board_name[board_id_str]
+
+    df = pd.read_csv(patient_data)
+    combined_boards[board_group] = df
+
+    df.to_csv(f"{board_id}.csv", index=False)
+    print(f"Saved cleaned CSV for board '{board_id}' as {board_id}.csv")
+
+# In its current form the billing data == patient data
+patient_data = pd.read_csv('8585885825.csv')
+Census_data = pd.read_csv('8586310441.csv')
+Scheduling_Texas_data = pd.read_csv('9023723118.csv')
+Incoming_Referrals_Texas_data = pd.read_csv('9023703555.csv')
 
 def clean_patient_name(name): 
     if pd.isnull(name):
@@ -15,7 +50,7 @@ def clean_patient_name(name):
 
 patient_data['Item Name'] = patient_data['Item Name'].apply(clean_patient_name)
 
-def Patient_Itemized(patient_data):
+def Patient_Itemized(patient_data, timestamp):
     Charge_codes = ['numeric_mkng945v', 'numeric_mkng1ekp', 'numeric_mkngdap1', 'numeric_mkngbb06',
                 'numeric_mknghpcs', 'numeric_mkng9d2s', 'numeric_mkng6bmr', 'numeric_mkqnk160', 'numeric_mkqn9t3r']
 
@@ -34,10 +69,15 @@ def Patient_Itemized(patient_data):
 
     collapsed_Patient_Name = collapsed_Patient_Name.sort_values(by='Total_Billed', ascending=False)
 
-    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H")
     collapsed_Patient_Name.to_csv(f"Patient_Itemized_{timestamp}.csv", index=True)
 
-def Provider_Itemized(patient_data):
+    # Making subset for patients that have an outstanding note 
+    Patient_NoNote = collapsed_Patient_Name[collapsed_Patient_Name['No Note on File'] != 0]
+    Patient_NoNote = Patient_NoNote.sort_values(by='No Note on File', ascending=False)
+    Patient_NoNote.to_csv(f"Patient_No_Note_{timestamp}.csv", index=True)
+    return collapsed_Patient_Name
+
+def Provider_Itemized(patient_data, timestamp):
     Charge_codes = ['numeric_mkng945v', 'numeric_mkng1ekp', 'numeric_mkngdap1', 'numeric_mkngbb06',
                 'numeric_mknghpcs', 'numeric_mkng9d2s', 'numeric_mkng6bmr', 'numeric_mkqnk160', 'numeric_mkqn9t3r']
 
@@ -49,20 +89,80 @@ def Provider_Itemized(patient_data):
 
     billed_sums = patient_data.groupby('Provider_Name')['Total_Billed'].sum().to_frame(name='Total_Billed')
 
+
     collapsed_Provider_Name = Group_counts.merge(billed_sums, left_index=True, right_index=True)
 
+    # Reorders the df to have total billed after the patient name
     cols = ['Total_Billed'] + [col for col in collapsed_Provider_Name.columns if col != 'Total_Billed']
     collapsed_Provider_Name = collapsed_Provider_Name[cols]
 
+    # Sorts in a desc order based on Total Billed
     collapsed_Provider_Name = collapsed_Provider_Name.sort_values(by='Total_Billed', ascending=False)
 
-    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H")
     collapsed_Provider_Name.to_csv(f"Provider_Itemized_{timestamp}.csv", index=True)
 
-def __main__():
-    Provider_Itemized(patient_data)
-    Patient_Itemized(patient_data)
+    Provider_NoNote = collapsed_Provider_Name[collapsed_Provider_Name['No Note on File'] != 0]
+    Provider_NoNote = Provider_NoNote.sort_values(by='No Note on File', ascending=False)
+    Provider_NoNote.to_csv(f"Provider_No_Note_{timestamp}.csv", index=True)
 
+def Marketer_Itemized(Incoming_Referrals_Texas_data, timestamp):
+    Incoming_Referrals_Texas = Capitalizing_ALL(Incoming_Referrals_Texas_data)
+    Incoming_Referrals_Texas['multiple_person_mkqagt70'] = Incoming_Referrals_Texas['multiple_person_mkqagt70'].fillna('NA')
+
+    Incoming_Referrals_Texas = Incoming_Referrals_Texas.rename(columns={'multiple_person_mkqagt70': 'Marketer_Name'})
+
+    # Collapses back down to just the number of patients by each marketer
+    Marketer_Sums = Incoming_Referrals_Texas.groupby('Marketer_Name')['Item Name'].count().to_frame(name='Total_Patients')
+
+    Marketer_Sums.to_csv(f"Marketer_Sums_{timestamp}.csv", index=True)
+    Incoming_Referrals_Texas = Incoming_Referrals_Texas.rename(columns={'Item Name': 'Patient_Name'})
+    return Incoming_Referrals_Texas
+
+def Capitalizing_ALL(Incoming_Referrals_Texas_data):
+    Incoming_Referrals_Texas_data['Item Name'] = Incoming_Referrals_Texas_data['Item Name'].str.upper().str.replace(' ', '', regex=False)
+    return Incoming_Referrals_Texas_data
+
+def Bridging_Marketer_Patient(Incoming_Referrals_Texas, collapsed_Patient_Name, timestamp):  
+    # Merges the Marketer info from Referrals into the billing set of patient data
+    Bridge_df = collapsed_Patient_Name.merge(Incoming_Referrals_Texas, on = 'Patient_Name', how = 'left')
+
+    Bridge_df.to_csv(f"Bridged_collapsed_Patient_{timestamp}.csv", index=True)
+    Marketer_Billed = Bridge_df.groupby('Marketer_Name')['Total_Billed'].count().to_frame(name='Total_Billed')
+    Marketer_Billed.to_csv(f"WoundSize_By_Marketer_{timestamp}.csv", index=True)
+    return Bridge_df
+
+def Census_Board_Import(Census_data, Referrals_Billing, timestamp):
+    Census_data= Capitalizing_ALL(Census_data)
+
+    Census_data = Census_data.rename(columns={'Item Name': 'Patient_Name'})
+    Bridge_df = Census_data.merge(Referrals_Billing, on = 'Patient_Name', how = 'left')
+    Bridge_df.to_csv(f"Overall_{timestamp}.csv", index=True)
+    Marketer_WoundSize = Bridge_df.groupby('Marketer_Name')['numeric_mknjsfxc'].sum().to_frame(name='Total_WoundArea')
+    
+    Marketer_WoundSize.to_csv(f"Total_Billed_Marketer_{timestamp}.csv", index=True)
+
+def final_folder_cleaning(timestamp):
+    folder_path = "/home/tym/Trinity_Mobile_Export/ty"
+    exceptions = {f"Marketer_Sums_{timestamp}.csv", f"Patient_Itemized_{timestamp}.csv", f"Patient_No_Note_{timestamp}.csv",f"Provider_Itemized_{timestamp}.csv",f"Bridged_collapsed_Patient_{timestamp}.csv",f"Overall_{timestamp}.csv"}
+
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        
+        if filename.endswith(".csv") and filename not in exceptions:
+            try:
+                os.remove(file_path)
+                print(f"Deleted: {filename}")
+            except Exception as e:
+                print(f"Failed to delete {filename}: {e}")
+
+def __main__():
+    Provider_Itemized(patient_data, timestamp)
+    collapsed_Patient_Name = Patient_Itemized(patient_data, timestamp)
+    Incoming_Referrals_Texas = Marketer_Itemized(Incoming_Referrals_Texas_data, timestamp)
+    Bridging_Marketer_Patient(Incoming_Referrals_Texas, collapsed_Patient_Name, timestamp)
+    Referrals_Billing = Bridging_Marketer_Patient(Incoming_Referrals_Texas, collapsed_Patient_Name, timestamp)
+    Census_Board_Import(Census_data, Referrals_Billing, timestamp)
+    final_folder_cleaning(timestamp)
 
 if __name__ == "__main__":
     __main__()
--- request_Script.py
+++ request_Script.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import json
 
 API_key = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjUyNTg3NjcwMCwiYWFpIjoxMSwidWlkIjo3NTE3MTU4NCwiaWFkIjoiMjAyNS0wNi0xM1QwMDo0MDo0Ny4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjY5NzE0OTYsInJnbiI6InVzZTEifQ.V_Gn7B-YJWoqeRiTKOX-tbrd2Ex7gDBPgs5COhVjFmw'
-board_id = '8585885825'
 url = "https://api.monday.com/v2"
 
 HEADERS = {
@@ -62,8 +61,6 @@ def fetch_board_items(board_id):
         if not cursor:
             break
         
-        # Delays to prevent overloading Monday
-        time.sleep(0.2)
 
     rows = []
     for item in all_items:
@@ -75,14 +72,25 @@ def fetch_board_items(board_id):
     return pd.DataFrame(rows)
 
 
-def export_to_csv(df):
+def export_to_csv(df, board_id):
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    df.to_csv(f"monday_export_{timestamp}.csv", index=False)
-    print(f"Exported {len(df)} items at {timestamp}")
+    df.to_csv(f"monday_export_{board_id}_{timestamp}.csv", index=False)
+    csv_timestamp = f"monday_export_{board_id}_{timestamp}.csv"
+    print(f"Exported {len(df)} items at {timestamp} for board # {board_id}")
+    return csv_timestamp, df
 
 def main():
-    df = fetch_board_items(board_id)
-    export_to_csv(df)
+    monday_boards = []
+    # Boards of interest are 8585885825 which is Billing, 8586310441 which is the Census Board, 9023723118 which is Scheduling - Texas, 9023703555 is Incoming Referrals - Texas, 
+    board_id = '8585885825', '8586310441', '9023723118', '9023703555'
+
+    for board_id in board_id:
+        df = fetch_board_items(board_id)
+        current_TimeStamp, df = export_to_csv(df, board_id)
+        monday_boards.append((board_id, current_TimeStamp))
+
+    return monday_boards
+
 
 if __name__ == "__main__":
     main()
